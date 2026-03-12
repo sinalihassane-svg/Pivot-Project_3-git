514487059d520844fec8af579fb43987bc5e2d6c
Mounirou-H-ops <habibmounirou2@gmail.com>
2026-03-12 10:53:28 +0000

    .

--- src/data_processing.py
+++ src/data_processing.py
@@ -52,34 +52,3 @@ disp.plot(cmap='Blues')
 plt.title("Matrice de Confusion sur l'ensemble de Test (20%)")
 plt.show()
 
-import pandas as pd
-import numpy as np
-
-def optimize_memory(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Optimise la mémoire du DataFrame en ajustant les types de données (ex: float64 -> float32).
-    """
-    start_mem = df.memory_usage().sum() / 1024**2
-    print(f'Mémoire initiale : {start_mem:.2f} MB')
-
-    for col in df.columns:
-        col_type = df[col].dtype
-
-        if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-            else:
-                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-
-    end_mem = df.memory_usage().sum() / 1024**2
-    print(f'Mémoire finale : {end_mem:.2f} MB')
-    return df
