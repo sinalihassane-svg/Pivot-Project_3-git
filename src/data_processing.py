48150d867643031ce474dfa18dbac1995bb54f8f
Mounirou-H-ops <habibmounirou2@gmail.com>
2026-03-15 20:28:01 +0000

    modif

--- src/data_processing.py
+++ src/data_processing.py
@@ -134,4 +134,34 @@ joblib.dump(scaler, os.path.join(dossier_modeles, 'modele_scaler.pkl'))
 joblib.dump(list(X_train_imputed.columns), os.path.join(dossier_modeles, 'modele_columns.pkl'))
 
 print(f"✅ Scaler sauvegardé avec succès dans : {dossier_modeles}")
+
+
+def optimize_memory(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Optimise la mémoire du DataFrame en ajustant les types de données (ex: float64 -> float32).
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print(f'Mémoire initiale : {start_mem:.2f} MB')
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+            else:
+                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print(f'Mémoire finale : {end_mem:.2f} MB')
+    return df
 # %%
